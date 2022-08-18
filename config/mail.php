451f582ecbf37e7a7d1b89447a257c5e57525994
developer1@imaris.ua <k.makienko1990@gmail.com>
2022-08-18 23:07:23 +0300

    mailgun - change email or password

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@api.comfort-health.net'),
+        'name' => env('MAIL_FROM_NAME', 'Enspirations'),
     ],
 
     /*
