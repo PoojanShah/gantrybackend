a04129c7989ba5f3f5e2b89a72afced3a3a7b44b
Andrii Derevianko <andreyy.derevjanko@gmail.com>
2022-09-26 10:13:22 +0300

    Added Models and endpoint with subscription by installation id

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::get('media', '\App\Http\Controllers\Api\VideoController@getMedia');
 Route::get('videos', '\App\Http\Controllers\Api\VideoController@getVideos');
 Route::get('messages', '\App\Http\Controllers\Api\VideoController@getMessages');
 Route::post('subscriptions', '\App\Http\Controllers\Api\SubscriptionController@createOrUpdate')->middleware('zoho.hook.auth');
-Route::get('installation/{installation_id}/subscription', '\App\Http\Controllers\Api\SubscriptionController@getClientSubscription');
+Route::get('installations/{installation_id}/subscription', '\App\Http\Controllers\Api\SubscriptionController@getClientSubscription');
